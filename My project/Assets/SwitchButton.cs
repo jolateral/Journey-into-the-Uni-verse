d45d2fa687912d7e4a8070054453f65fdf14491b
elena-ding <emailelenading@gmail.com>
2024-10-27 01:45:05 -0400

    update checklist with changing buttons

--- My project/Assets/SwitchButton.cs
+++ My project/Assets/SwitchButton.cs
@@ -4,20 +4,20 @@ using TMPro;
 
 public class SwitchButton : MonoBehaviour
 {
-    public GameObject image1; 
-    public GameObject image2;
-    
-    public void ChangeImage()
+    public Sprite newButtonImage;
+    public Button button;
+    void Start()
     {
-        if (image1.activeSelf)
-        {
-            image1.SetActive(false);  // Deactivate Image1
-            image2.SetActive(true);   // Activate Image2
-        }
-        else
-        {
-            image1.SetActive(true);   // Activate Image1
-            image2.SetActive(false);  // Deactivate Image2
-        }
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ChangeButtonImage()
+    {
+        button.image.sprite = newButtonImage;
     }
 }
